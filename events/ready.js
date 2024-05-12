430f1b78f90cc56c074c6c1701e6d385e440a2b2
Eric <168204149+J72s@users.noreply.github.com>
2024-05-12 15:40:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const utils = require('../utils');
 
 module.exports = client => {
 
-    client.user.setActivity("gud music", {type: "LISTENING"});
+    client.user.setActivity("Eric music", {type: "LISTENING"});
 
     utils.log(`Logged in as ${client.user.username} !`);
 
